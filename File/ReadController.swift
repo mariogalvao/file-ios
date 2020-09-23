996ff6a326087db3a0acb2942b2eb63086d54c2d
Mario Galvão <mcasarigalvao@gmail.com>
2020-09-22 22:05:00 -0300

    filemanager

--- File/ReadController.swift
+++ File/ReadController.swift
@@ -10,25 +10,66 @@ import UIKit
 class ReadController: UIViewController {
     
     @IBOutlet weak var textFieldKey: UITextField!
+    @IBOutlet weak var labelKey: UILabel!
+    @IBOutlet weak var labelValue: UILabel!
     @IBOutlet weak var labelNoValue: UILabel!
     @IBOutlet weak var stackViewValue: UIStackView!
-    @IBOutlet weak var labelValue: UILabel!
+    @IBOutlet weak var labelFileContent: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        clear()
+    }
+    
+    private func clear() {
         labelNoValue.isHidden = true
         stackViewValue.isHidden = true
         textFieldKey.text = nil
-        labelValue.text = nil
+        labelFileContent.text = nil
     }
-
+    
+    @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
+        clear()
+        if sender.selectedSegmentIndex == 0 {
+            labelKey.text = "Key"
+            labelValue.text = "Value:"
+        } else {
+            labelKey.text = "Filename"
+            labelValue.text = "File Text:"
+        }
+    }
+    
     @IBAction func search(_ sender: UIButton) {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            searchValue()
+        } else {
+            searchFile()
+        }
+    }
+    
+    private func searchValue() {
         if let value = UserDefaults.standard.value(forKey: textFieldKey.text ?? "") as? String {
             showValue(value)
         } else {
             showNoValue()
         }
+    }
+    
+    private func searchFile() {
+        let filename = getDocumentsDirectory().appendingPathComponent("\(textFieldKey.text ?? "").txt")
+        do {
+            let fileText = try String(contentsOf: filename, encoding: .utf8)
+            showValue(fileText)
+        } catch {
+            showNoValue()
+        }
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
     private func showNoValue() {
@@ -39,7 +80,7 @@ class ReadController: UIViewController {
     private func showValue(_ value: String) {
         labelNoValue.isHidden = true
         stackViewValue.isHidden = false
-        labelValue.text = value
+        labelFileContent.text = value
     }
 
 }
--- File/WriteController.swift
+++ File/WriteController.swift
@@ -9,14 +9,64 @@ import UIKit
 
 class WriteController: UIViewController {
     
+    @IBOutlet weak var labelKey: UILabel!
+    @IBOutlet weak var labelValue: UILabel!
     @IBOutlet weak var textFieldKey: UITextField!
     @IBOutlet weak var textFieldValue: UITextField!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     
-    @IBAction func write(_ sender: UIButton) {
-        UserDefaults.standard.setValue(textFieldValue.text ?? "",
-                                       forKey: textFieldKey.text ?? "")
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        clear()
+    }
+    
+    private func clear() {
         textFieldKey.text = nil
         textFieldValue.text = nil
+    }
+    
+    @IBAction func segmentedControlChanged(_ sender: UISegmentedControl) {
+        clear()
+        if sender.selectedSegmentIndex == 0 {
+            labelKey.text = "Key"
+            labelValue.text = "Value"
+        } else {
+            labelKey.text = "Filename"
+            labelValue.text = "File Text"
+        }
+    }
+    
+    
+    @IBAction func write(_ sender: UIButton) {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            writeValue()
+        } else {
+            writeFile()
+        }
+        textFieldKey.text = nil
+        textFieldValue.text = nil
+    }
+    
+    private func writeValue() {
+        UserDefaults.standard.setValue(textFieldValue.text ?? "",
+                                       forKey: textFieldKey.text ?? "")
+    }
+    
+    private func writeFile() {
+        let str = textFieldValue.text ?? ""
+        let filename = getDocumentsDirectory().appendingPathComponent("\(textFieldKey.text ?? "").txt")
+
+        do {
+            try str.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+        } catch {
+            print("Erro ao salvar arquivo.")
+        }
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
 }
